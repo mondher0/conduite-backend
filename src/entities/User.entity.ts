1ece3211c6f8f02adbbc35717ade5ce58e15bc95
mondher <110062316+mondher0@users.noreply.github.com>
2024-11-07 21:36:22 +0100

    update: removing create, update and delete date from the user entity

--- src/entities/User.entity.ts
+++ src/entities/User.entity.ts
@@ -18,22 +18,4 @@ export class User extends GenericEntity {
 
   @Column()
   password: string;
-
-  @CreateDateColumn({
-    name: "created_at",
-    type: "timestamp with time zone",
-    default: () => "CURRENT_TIMESTAMP",
-  })
-  createdAt: Date;
-
-  @UpdateDateColumn({
-    name: "updated_at",
-    type: "timestamp with time zone",
-    default: () => "CURRENT_TIMESTAMP",
-    onUpdate: "CURRENT_TIMESTAMP",
-  })
-  updatedAt: Date;
-
-  @DeleteDateColumn()
-  deletedAt?: Date;
 }
